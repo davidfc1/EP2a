f0c72840803a3420f9934853cb9fbd86aacec50d
Ian Faray <iansfa@al.insper.edu.br>
2022-11-13 19:31:23 -0300

    valida lista

---
+++ valida_lista.py
@@ -0,0 +1,85 @@
+def valida_questoes(lista):
+    final = []
+    for dic in lista:
+        res={}
+
+        titulo = True
+        nivel = True
+        opcoes = True
+        correta = True
+
+        if "titulo" not in dic:
+            res["titulo"]= "nao_encontrado"
+            titulo= False
+        if "nivel" not in dic:
+            res["nivel"]= "nao_encontrado"
+            nivel= False
+        if "opcoes" not in dic:
+            res["opcoes"]= "nao_encontrado"
+            opcoes= False
+        if "correta" not in dic:
+            res["correta"]= "nao_encontrado"
+            correta= False
+        if len(dic)!= 4:
+            res["outro"]= "numero_chaves_invalido"
+        if titulo == True:
+            if len(dic["titulo"])== 0 or dic["titulo"].isspace()== True:
+                res["titulo"]= 'vazio'
+        if nivel == True:
+            if dic["nivel"]!= "facil" and dic["nivel"]!= "medio" and dic["nivel"]!= "dificil":
+                res['nivel']= 'valor_errado'    
+        if opcoes== True: 
+            if len(dic["opcoes"])!= 4   :
+                res["opcoes"]='tamanho_invalido'
+        if opcoes== True: 
+            if len(dic["opcoes"])== 4 and "A" not in dic["opcoes"] or "B" not in dic["opcoes"] or "C" not in dic["opcoes"] or "D" not in dic["opcoes"] :
+                if 'opcoes'not in res:
+                    res['opcoes']='chave_invalida_ou_nao_encontrada'
+        
+        if opcoes == True and "A"  in dic["opcoes"] and len(dic["opcoes"])== 4 and  dic["opcoes"]['A'].isspace() == True :
+            if 'opcoes'not in res:
+                res["opcoes"]= {}
+                res["opcoes"]["A"]='vazia'  
+        if opcoes == True and "B"  in dic["opcoes"] and len(dic["opcoes"])== 4 and  dic["opcoes"]['B'].isspace() == True :
+            if 'opcoes'not in res:
+                res["opcoes"]= {}
+                res["opcoes"]["B"]='vazia'
+            else:
+                res["opcoes"]["B"]='vazia'
+        if opcoes== True and "C"  in dic["opcoes"] and len(dic["opcoes"])== 4 and  dic["opcoes"]['C'].isspace() == True :
+            if 'opcoes'not in res:
+                res["opcoes"]= {}
+                res["opcoes"]["C"]='vazia'
+            else:
+                res["opcoes"]["C"]='vazia'
+        if opcoes== True and "D"  in dic["opcoes"] and len(dic["opcoes"])== 4 and  dic["opcoes"]['D'].isspace() == True :
+            if 'opcoes'not in res:
+                res["opcoes"]= {}
+                res["opcoes"]["D"]='vazia'
+            else:
+                res["opcoes"]["D"]='vazia'
+
+        
+        if correta == True and dic['correta']!= 'A' and dic['correta']!= 'B' and dic['correta']!= 'C'  and dic['correta']!= 'D':
+            res['correta']= 'valor_errado'
+        
+        final.append(res)
+    
+    return final
+    
+
+
+    
+    
+        
+
+        
+        
+        
+            
+        
+    
+    
+
+    
+    
